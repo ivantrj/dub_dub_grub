676d7c50bec53ac749118b54df61ea77bdde8297
Ivan Trj <ivantrajanovski@gmail.com>
2021-11-24 15:58:49 +0100

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Ivan Trajanovski on 2021. 11. 24..
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
